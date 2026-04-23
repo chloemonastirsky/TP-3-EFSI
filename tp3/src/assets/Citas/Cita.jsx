1e744db3701a62bccc6f74ec02c434f2ddeaa555
CATALINA COHEN <49092736@ort.edu.ar>
2026-04-23 10:28:19 -0300

    queda completar App.jsx

--- tp3/src/assets/Citas/Cita.jsx
+++ tp3/src/assets/Citas/Cita.jsx
@@ -1,16 +1,20 @@
+import { useState } from 'react';
 import './Cita.css';
+import Formulario from '../Formulario/Formulario';
 
-const Cita = ({ cita, eliminarCita }) => {
+const Cita = ({ cita, listaMascotas, setListaMascotas}) => {
+    
     return (
         <div className="cita">
-            <ul>
-                <li><strong>Nombre Mascota:</strong> {cita.nombreMascota}</li>
-                <li><strong>Nombre Dueño:</strong> {cita.nombreDueño}</li>
-                <li><strong>Fecha:</strong> {cita.fecha}</li>
-                <li><strong>Hora:</strong> {cita.hora}</li>
-                <li><strong>Sintomas:</strong> {cita.sintomas}</li>
-            </ul>
-            <button onClick={() => eliminarCita(cita.id)}>Eliminar</button>
+            {listaMascotas.map(mascota => (
+                <li >
+                    <strong>Nombre Mascota:</strong> {mascota.nombreMascota}<br />
+                    <strong>Nombre Dueño:</strong> {mascota.nombreDueño}<br />
+                    <strong>Fecha:</strong> {mascota.fecha}<br />
+                    <strong>Hora:</strong> {mascota.hora}<br />
+                    <strong>Sintomas:</strong> {mascota.sintomas}
+                </li>
+            ))}
         </div>
     )
 }
--- tp3/src/assets/Formulario/Formulario.jsx
+++ tp3/src/assets/Formulario/Formulario.jsx
@@ -1,19 +1,68 @@
+import { useState } from "react";
+import './Formulario.css';
 const Formulario = () => {
+    const [listaMascotas, setListaMascotas] = useState([]);
+    const [nombreMascota, setNombreMascota]=useState("");
+    const [nombreDueño, setNombreDueño]=useState("");
+    const [fecha, setFecha]=useState("");
+    const [hora, setHora]=useState("");
+    const [sintomas, setSintomas]=useState("");
+     
+    const formularioSubmit = (e) => {
+        e.preventDefault();
+        const nuevaMascota = {
+            id: Date.now(),
+            nombreMascota,
+            nombreDueño,
+            fecha,
+            hora,
+            sintomas
+        };
+        setListaMascotas([...listaMascotas, nuevaMascota]);
+        setNombreMascota("");
+        setNombreDueño("");
+        setFecha("");
+        setHora("");
+        setSintomas("");
+    };
     return (
         <div className="formulario app__main">
 
-            <form>
+            <form onSubmit={formularioSubmit}>
                 <label htmlFor="nombreMascota">Nombre Mascota:</label>
-                <input type="text" id="nombreMascota" name="nombreMascota" required />
+                <input 
+                type="text" 
+                onKeyUp={(event)=> setNombreMascota(event.target.value)}
+            />
                 <label htmlFor="nombreDueño">Nombre Dueño:</label>
-                <input type="text" id="nombreDueño" name="nombreDueño" required />
+                <input 
+                type="text" 
+                onKeyUp={(event)=> setNombreDueño(event.target.value)}
+            />
                 <label htmlFor="date">Fecha:</label>
-                <input type="date" id="date" name="date" required />
+                <input 
+                type="text" 
+                onKeyUp={(event)=> setFecha(event.target.value)}
+            />
                 <label htmlFor="time">Hora:</label>
-                <input type="time" id="time" name="time" required />
+                <input 
+                type="text" 
+                onKeyUp={(event)=> setHora(event.target.value)}
+            />
+
                 <label htmlFor="sintomas">Sintomas:</label>
-                <input type="text" id="sintomas" name="sintomas" required />
-                <button type="submit">Agregar Cita</button>
+                <input 
+                type="text" 
+                onKeyUp={(event)=> setSintomas(event.target.value)}
+            />
+
+                <button 
+                onClick={() => {
+                setListaMascotas([
+                    ...listaMascotas,
+                    nombreMascota
+                ])
+                }}>Agregar Mascota</button>
             </form>
         </div>
     )
